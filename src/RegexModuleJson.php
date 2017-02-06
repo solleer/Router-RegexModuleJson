b509bfe49700549708aaa6591460d1d5614fa22a
Richard <solleer@hotmail.com>
2017-02-06 12:06:54 -0500

    Initial code and composer.json

---
+++ src/RegexModuleJson.php
@@ -0,0 +1,43 @@
+<?php
+namespace Config\Router;
+class RegexModuleJson implements \Level2\Router\Rule {
+    private $jsonModule;
+
+    public function __construct(ModuleJson $moduleJson) {
+        $this->moduleJson = $moduleJson;
+    }
+
+    public function find(array $route) {
+        if (count($route) == 0 || $route[0] == '') return false;
+        $moduleName = $route[0];
+
+        $config = $this->moduleJson->getConfig($route)->conditions;
+        $config = json_decode(json_encode($config), true);
+
+        $newRoute = $this->getRoute($route, $config);
+        $route = array_merge([$moduleName], $newRoute !== false ? $newRoute : $route);
+
+        return $this->moduleJson->find($route);
+    }
+
+    private function getRoute($route, $config) {
+        foreach ($config as $routeName => $routeRegex) {
+            $newRoute = $this->matchRoute($route, $routeRegex);
+            if ($newRoute !== false) {
+                if (empty($newRoute)) return $route;
+                else return array_merge([$routeName], $newRoute);
+            }
+        }
+        return false;
+    }
+
+    private function matchRoute(array $route, array $routeRegex) {
+        $newRoute = [];
+        foreach ($routeRegex as $key => $regex) {
+            $result = preg_match($regex, $route[$key], $matches);
+            if ($result !== 1) return false;
+            $newRoute = array_merge($newRoute, array_slice($matches, 1));
+        }
+        return $newRoute;
+    }
+}
